e51bdeaf54482e9c14f19ea160eb602968601b2d
TobiasBohnen <1294616+TobiasBohnen@users.noreply.github.com>
2024-12-23 17:59:13 +0100

    api update

--- BrickOut/B2DebugDraw.cpp
+++ BrickOut/B2DebugDraw.cpp
@@ -32,7 +32,7 @@ void B2DDebugDraw::draw_solid_polygon(physics::body_transform xform, std::span<p
 {
     _canvas.set_fill_style(color);
 
-    transform xform2;
+    gfx::transform xform2;
     xform2.rotate_at(xform.Angle, xform.Center);
 
     std::vector<point_f> verts;
