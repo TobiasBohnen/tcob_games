4e78ae81bba7d72c8b18103d7fa5e27ae95064b4
Tobias Bohnen <1294616+TobiasBohnen@users.noreply.github.com>
2025-08-05 17:00:57 +0200

    cleanup

--- FallingPixels/main.cpp
+++ FallingPixels/main.cpp
@@ -28,8 +28,7 @@ auto main(void) -> int
 #else
 auto main(int /* argc */, char* argv[]) -> int
 {
-    tcob::game game {{.Name          = "FallingPixels",
-                      .WorkerThreads = 16}};
+    tcob::game game {{.Name = "FallingPixels"}};
 #endif
 
     game.push_scene<main_scene>();
