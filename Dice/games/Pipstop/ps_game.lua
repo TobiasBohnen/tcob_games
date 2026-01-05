394cb59a6700818ecc59f4c5730429ec0df6298b
Tobias Bohnen <1294616+TobiasBohnen@users.noreply.github.com>
2026-01-05 13:17:03 +0100

    Dice: wip

--- Dice/games/Pipstop/ps_game.lua
+++ Dice/games/Pipstop/ps_game.lua
@@ -2,38 +2,78 @@
 -- MIT License
 -- https://opensource.org/licenses/MIT
 
-local DURATION = 5500
+local DURATION       = 5500
+local SEGMENT_LENGTH = 300
 
-local gfx      = require('ps_gfx')
-local sfx      = require('ps_sfx')
+local gfx            = require('ps_gfx')
+local sfx            = require('ps_sfx')
 
-local game     = {
-    sockets  = {}, ---@type { [string]: socket }
+local game           = {
+    sockets         = {}, ---@type { [string]: socket }
 
-    textures = {
+    textures        = {
     },
 
-    sounds   = {
+    sounds          = {
     },
+
+    track           = {
+        0, 0,
+        0.5, -0.5, 0.5,
+        0, 0,
+        -0.8, -0.8, -0.8,
+        0, 0,
+        0.3, 0.3,
+        -0.3, -0.3,
+        0, 0, 0,
+        1.0, 1.0, 1.0,
+        0.5,
+        0, 0,
+        -0.5, -0.5,
+        0, 0, 0,
+        0.7, 0.7,
+        -0.7, -0.7,
+        0, 0, 0
+    },
+    trackIndex      = 1,
+    segmentProgress = 0,
 }
 
 ---@param engine engine
 function game:on_setup(engine)
-    gfx.create_background(engine, 0, 0)
+    gfx.create_background(engine, 0, 0, 1)
     gfx.create_textures(self, engine)
     engine:create_sounds(sfx.get_sounds(self, engine))
 end
 
+local x = 0
+
 ---@param engine engine
 function game:on_turn_start(engine)
+    x = x % 4 + 1
+end
 
+function game:get_curve(speed)
+    self.segmentProgress = self.segmentProgress + speed
+
+    if self.segmentProgress >= SEGMENT_LENGTH then
+        self.segmentProgress = self.segmentProgress - SEGMENT_LENGTH
+        self.trackIndex = (self.trackIndex % #self.track) + 1
+    end
+
+    local currentCurve = self.track[self.trackIndex]
+    local nextCurve    = self.track[(self.trackIndex % #self.track) + 1]
+
+    local t            = self.segmentProgress / SEGMENT_LENGTH
+    return currentCurve + (nextCurve - currentCurve) * t
 end
 
 ---@param engine engine
 ---@param deltaTime number
 function game:on_turn_update(engine, deltaTime, turnTime)
     if turnTime >= DURATION then return GameStatus.TurnEnded end
-    gfx.create_background(engine, (turnTime / DURATION) * 2 - 1, ((turnTime / DURATION) * 3) % 1)
+
+    gfx.create_background(engine, self:get_curve(4), self.segmentProgress / SEGMENT_LENGTH, x)
     --  if self.ship.health == 0 then return GameStatus.GameOver end
     return GameStatus.Running
 end
--- Dice/games/Pipstop/ps_gfx.lua
+++ Dice/games/Pipstop/ps_gfx.lua
@@ -2,28 +2,151 @@
 -- MIT License
 -- https://opensource.org/licenses/MIT
 
-local gfx              = {}
+local gfx            = {}
 
-local HORIZON_HEIGHT   = 100
+local HORIZON_HEIGHT = 100
 
-local SKY_COLOR        = Palette.Blue
-local GROUND_COLOR     = Palette.Green
-local ROAD_COLOR       = Palette.Gray
-local CENTERLINE_COLOR = Palette.White
-local CURB_COLOR1      = Palette.Red
-local CURB_COLOR2      = Palette.White
+local COLORS         = {
+    normal = {
+        sky          = Palette.Blue,
+        horizonLine1 = Palette.LightBlue,
+        horizonLine2 = Palette.White,
+        sun          = Palette.Yellow,
+        cloud        = Palette.White,
+        city         = Palette.Gray,
+        ground       = Palette.Green,
+        road         = Palette.Gray,
+        centerLine   = Palette.White,
+        curb1        = Palette.Red,
+        curb2        = Palette.White
+    },
+    winter = {
+        sky          = Palette.LightBlue,
+        horizonLine1 = Palette.White,
+        horizonLine2 = Palette.White,
+        sun          = Palette.Yellow,
+        cloud        = Palette.White,
+        city         = Palette.DarkBlue,
+        ground       = Palette.White,
+        road         = Palette.Gray,
+        centerLine   = Palette.White,
+        curb1        = Palette.Red,
+        curb2        = Palette.White
+    },
+    dusk = {
+        sky          = Palette.BlueGray,
+        horizonLine1 = Palette.DarkBlue,
+        horizonLine2 = Palette.Blue,
+        sun          = Palette.Red,
+        cloud        = Palette.Gray,
+        city         = Palette.Black,
+        ground       = Palette.DarkGreen,
+        road         = Palette.BlueGray,
+        centerLine   = Palette.Gray,
+        curb1        = Palette.Brown,
+        curb2        = Palette.Gray
+    },
+    fall = {
+        sky          = Palette.DarkBlue,
+        horizonLine1 = Palette.Blue,
+        horizonLine2 = Palette.Yellow,
+        sun          = Palette.Orange,
+        cloud        = Palette.White,
+        city         = Palette.BlueGray,
+        ground       = Palette.DarkBrown,
+        road         = Palette.Gray,
+        centerLine   = Palette.White,
+        curb1        = Palette.Red,
+        curb2        = Palette.White
+    }
+}
+
+local worldRotation  = 0
+
+local city           = nil
+local clouds         = nil
 
 ---@param engine engine
-function gfx.create_background(engine, curveAmount, trackOffset)
-    local bg   = engine.bg
-    local size = bg.size
-    local w, h = size.width, size.height
+function gfx.create_background(engine, curveAmount, trackOffset, theme)
+    local bg          = engine.bg
+    local size        = bg.size
+    local w, h        = size.width, size.height
+
+    local themes      = { "normal", "dusk", "fall", "winter" }
+    local colors      = COLORS[themes[theme]]
+
+    local WORLD_WIDTH = w * 4
+    local SUN_SPEED   = 0.5
+    local CITY_SPEED  = 0.8
+    local CLOUD_SPEED = 1.5
+
+    worldRotation     = (worldRotation + curveAmount) % WORLD_WIDTH
+
+    local function get_draw_x(objectX, speedMult)
+        local span = WORLD_WIDTH * speedMult
+        local offset = worldRotation * speedMult
+        return ((objectX - offset + span / 2) % span) - span / 2
+    end
 
     -- sky
-    bg:rect({ x = 0, y = 0, width = w, height = HORIZON_HEIGHT }, SKY_COLOR, true)
+    bg:rect({ x = 0, y = 0, width = w, height = HORIZON_HEIGHT }, colors.sky, true)
+    bg:rect({ x = 0, y = HORIZON_HEIGHT - 3, width = w, height = 2 }, colors.horizonLine1, true)
+    bg:rect({ x = 0, y = HORIZON_HEIGHT - 1, width = w, height = 1 }, colors.horizonLine2, true)
+
+    -- sun
+    local sunX    = get_draw_x(30, SUN_SPEED)
+    local sunSpan = WORLD_WIDTH * SUN_SPEED
+    local sunWrap = (sunX > 0) and (sunX - sunSpan) or (sunX + sunSpan)
+
+    bg:circle({ x = sunX, y = 20 }, 10, colors.sun, true)
+    bg:circle({ x = sunWrap, y = 20 }, 10, colors.sun, true)
+
+    -- city
+    if not city then
+        city = {}
+        for i = 1, 15 do
+            local bw = engine:rnd(3, 10)
+            local bh = engine:rnd(12, 20)
+            table.insert(city, {
+                x = engine:rnd(0, 1) * 120,
+                w = bw,
+                h = bh
+            })
+        end
+    end
+
+    local citySpan = WORLD_WIDTH * CITY_SPEED
+    for _, b in ipairs(city) do
+        local drawX = get_draw_x(b.x, CITY_SPEED)
+        local wrapX = (drawX > 0) and (drawX - citySpan) or (drawX + citySpan)
+        local yPos  = HORIZON_HEIGHT - b.h + 1
+        bg:rect({ x = drawX, y = yPos, width = b.w, height = b.h }, colors.city, true)
+        bg:rect({ x = wrapX, y = yPos, width = b.w, height = b.h }, colors.city, true)
+    end
+
+    -- clouds
+    if not clouds then
+        clouds = {}
+        for i = 1, 25 do
+            local cx = engine:rnd(0, 1) * (WORLD_WIDTH * CLOUD_SPEED)
+            local cy = math.floor(engine:rnd(0, 1) * HORIZON_HEIGHT / 4 * 3)
+            local cw = engine:irnd(12, 22)
+            local ch = engine:irnd(3, 6)
+            table.insert(clouds, { x = cx, y = cy, width = cw, height = ch })
+        end
+    end
+
+    local cSpan = WORLD_WIDTH * CLOUD_SPEED
+    for idx, cloud in ipairs(clouds) do
+        local drawX = get_draw_x(cloud.x, CLOUD_SPEED)
+        local wrapX = (drawX > 0) and (drawX - cSpan) or (drawX + cSpan)
+
+        bg:rect({ x = drawX, y = cloud.y, width = cloud.width, height = cloud.height }, colors.cloud, true)
+        bg:rect({ x = wrapX, y = cloud.y, width = cloud.width, height = cloud.height }, colors.cloud, true)
+    end
 
     -- ground
-    bg:rect({ x = 0, y = HORIZON_HEIGHT, width = w, height = h - HORIZON_HEIGHT }, GROUND_COLOR, true)
+    bg:rect({ x = 0, y = HORIZON_HEIGHT, width = w, height = h - HORIZON_HEIGHT }, colors.ground, true)
 
     -- road
     for y = HORIZON_HEIGHT, h - 1 do
@@ -38,7 +161,7 @@ function gfx.create_background(engine, curveAmount, trackOffset)
         local leftEdge      = centerX - math.floor(roadWidth / 2)
         local rightEdge     = centerX + math.floor(roadWidth / 2)
 
-        bg:line({ x = leftEdge, y = y }, { x = rightEdge, y = y }, ROAD_COLOR)
+        bg:line({ x = leftEdge, y = y }, { x = rightEdge, y = y }, colors.road)
 
         -- stripes
         local stripeWidth = math.max(0.5, roadWidth / 50)
@@ -46,14 +169,14 @@ function gfx.create_background(engine, curveAmount, trackOffset)
         local distance    = (((math.log(1 + t * 10) / math.log(11)) - trackOffset) * 100) % 100
 
         if distance % 30 < 20 then
-            bg:line({ x = math.ceil(centerX - stripeWidth), y = y }, { x = math.floor(centerX + stripeWidth), y = y }, CENTERLINE_COLOR)
+            bg:line({ x = math.ceil(centerX - stripeWidth), y = y }, { x = math.floor(centerX + stripeWidth), y = y }, colors.centerLine)
         end
         if distance % 20 < 10 then
-            bg:line({ x = leftEdge - stripeWidth, y = y }, { x = leftEdge + stripeWidth, y = y }, CURB_COLOR1)
-            bg:line({ x = rightEdge - stripeWidth, y = y }, { x = rightEdge + stripeWidth, y = y }, CURB_COLOR1)
+            bg:line({ x = leftEdge - stripeWidth, y = y }, { x = leftEdge + stripeWidth, y = y }, colors.curb1)
+            bg:line({ x = rightEdge - stripeWidth, y = y }, { x = rightEdge + stripeWidth, y = y }, colors.curb1)
         else
-            bg:line({ x = leftEdge - stripeWidth, y = y }, { x = leftEdge + stripeWidth, y = y }, CURB_COLOR2)
-            bg:line({ x = rightEdge - stripeWidth, y = y }, { x = rightEdge + stripeWidth, y = y }, CURB_COLOR2)
+            bg:line({ x = leftEdge - stripeWidth, y = y }, { x = leftEdge + stripeWidth, y = y }, colors.curb2)
+            bg:line({ x = rightEdge - stripeWidth, y = y }, { x = rightEdge + stripeWidth, y = y }, colors.curb2)
         end
     end
 end
